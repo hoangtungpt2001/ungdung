f783aa43a53c76a565fa3378f7e7cfe9e4551305
hoangtungpt2001 <hoangtungpt2001@gmail.com>
2022-10-11 10:20:30 +0700

    first commit

---
+++ bai2.1/src/main/java/vd2.java
@@ -0,0 +1,27 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class vd2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");  
+		PrintWriter pw=response.getWriter();  
+		  
+		String name=request.getParameter("name");//will return value  
+		pw.println("Welcome "+name);  
+		  
+		pw.close();  
+	}
+
+}
